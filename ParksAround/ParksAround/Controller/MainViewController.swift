953278e0c50aadb1521bdaa8ff41b6267a7904ce
AndreyPrez <aw06.1994@yandex.ru>
2022-06-08 01:36:42 +0300

    parkNames added and dislayed

--- ParksAround/ParksAround/Controller/MainViewController.swift
+++ ParksAround/ParksAround/Controller/MainViewController.swift
@@ -8,82 +8,30 @@
 import UIKit
 
 class MainViewController: UITableViewController {
+    
+    private var parkNames = [
+        "Парк культуры и отдыха им. М. Горького", "Нескучный сад", "Парк «Зарядье»", "ВДНХ", "Парк «Сокольники»", "Парк «Останкино»", "Парк «Ходынское поле»", "Парк искусств «Музеон»", "Парк «Тюфелева роща»", "Парк-заповедник «Царицыно»", "Парк-заповедник «Коломенское»", "Парк Победы на Поклонной горе", "Парк «Новодевичьи пруды»", "Александровский сад", "Парк «Патриот»", "Парк «Красная Пресня»", "Гончаровский парк", "Измайловский парк", "Парк «Фили»", "Суворовский парк", "Воронцовский парк", "Парк «Кузьминки-Люблино»", "Парк «Садовники»", "Перовский парк", "Парк «Мещерский»", "Парк 50-летия Октября", "Парк музея-усадьбы «Кусково»", "Парк «Бабушкинский»", "Парк Дружбы", "Лианозовский парк", "Парк «Дубки»", "Парк «Алтуфьево»", "Парк «Ростокинский Акведук»", "Патриаршие пруды", "Парк возле главного здания МГУ им. Ломоносова"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+    
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return parkNames.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        var cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = parkNames[indexPath.row]
+        
+        return cell ?? UITableViewCell()
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
